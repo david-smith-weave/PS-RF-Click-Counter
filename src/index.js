f269b37841f245b2e02bcbe2803ebbd4bb36a1fa
David Smith <david.smith@weaveability.com>
2019-12-04 10:03:46 +0000

    new default value for clicks

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let model = { clicks: 0 };
+let model = { clicks: 5 };
 ReactDOM.render(<App clicks={model.clicks} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
